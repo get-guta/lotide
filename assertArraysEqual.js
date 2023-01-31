4fcd22f017768babe3bc7923196a2e3574c2a691
getenesh <getenesh.guta@gmail.com>
2023-01-31 17:49:26 -0500

    Caaling a function inside a function.

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) throw Error('params must be typeof array');
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    } 
+  }
+  return true
+};
+
+const assertArraysEqual = function(array1, array2){
+  if(eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed:  ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
