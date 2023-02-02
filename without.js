f259a79c184838ca9c4f5957d383a660242eb5d5
getenesh <getenesh.guta@gmail.com>
2023-02-02 17:14:34 -0500

    wothout

---
+++ without.js
@@ -0,0 +1,37 @@
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    } 
+  }
+  return true
+};
+
+const without = function (array, excludeNums) {
+  let selected = [];
+  for (const arr of array) {
+    if (!excludeNums.includes(arr)){
+      selected.push(arr);
+    }
+  }
+  return selected;
+}
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+
+const assertArraysEqual = function(array1, array2){
+ if(eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed:  ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "4"]); // => ["1", "2"]
+
+
+
