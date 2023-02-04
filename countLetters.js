11f5194dda93b3aca5c6b6a9ac38905b8f49a69f
getenesh <getenesh.guta@gmail.com>
2023-02-04 18:25:09 -0500

    counts number of character in a sentence.

---
+++ countLetters.js
@@ -0,0 +1,18 @@
+const countLetters = function (sentence) {
+  //newWord = sentence.split(" ");
+  const wordCount = {};
+  for (let char of sentence) {
+    // console.log(char);
+    if(char !== " "){
+      if (wordCount[char]) {
+        wordCount[char] += 1;
+      } else {
+        wordCount[char] = 1;
+      }
+     }
+    }
+  return wordCount
+
+};
+
+console.log(countLetters("lighthouse in the house"));
