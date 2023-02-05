8db8be82f3b50a47136ed6c0d6b757b20ba0d824
getenesh <getenesh.guta@gmail.com>
2023-02-04 22:29:13 -0500

    finds keys by value

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+
+function findKeyByValue(object, value) {
+  return Object.keys(object).find(
+    function(key) {return object[key] === value;
+  });
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual(findKeyByValue({a:2, b:5, c:6}, 5), "b");
