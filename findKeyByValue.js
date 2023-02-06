1cca5803a7afa092b019d9ad3b73cc83aca63d4e
getenesh <getenesh.guta@gmail.com>
2023-02-06 14:38:22 -0500

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+function findKeyByValue(object, value) {
+  let keys = Object.keys(object);
+  for(let key of keys){
+    if(object[key] === value){
+      return key;
+    }
+
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
