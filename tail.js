00a23f4dc82ef98e7a029822674546f6cec7cbed
getenesh <getenesh.guta@gmail.com>
2023-02-15 21:39:38 -0500

    Refactored tail

--- tail.js
+++ tail.js
@@ -7,13 +7,5 @@ const tail = function(arr) {
   return newArr;
 
 };
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]);
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 module.exports = tail;
