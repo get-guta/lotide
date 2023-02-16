f1382b154c37cadd49d57bcc3183025632ddba30
getenesh <getenesh.guta@gmail.com>
2023-02-15 20:10:51 -0500

    Refactored Middle

--- middle.js
+++ middle.js
@@ -1,19 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed:  ${array1} === ${array2}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed:  ${array1} !== ${array2}`);
-  }
-};
 
 const middle = function (arr) {
   let mid;
@@ -29,7 +13,6 @@ const middle = function (arr) {
   return res;
 };
 
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3])); 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4,]));
+module.exports = middle;
+
+
